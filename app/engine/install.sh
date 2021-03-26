2795d1b9f5b30da085e7703f174c2439e7728c1b
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2021-03-26 14:53:15 +0530

    Update install.sh

--- app/engine/install.sh
+++ app/engine/install.sh
@@ -1,6 +1,3 @@
-#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1N7LVLbxuL67_irmuSmJp_WRmSDHxanu5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1N7LVLbxuL67_irmuSmJp_WRmSDHxanu5" -O checkpoint.zip && rm -rf /tmp/cookies.txtpip install gpt-2-simple
-#unzip checkpoint.zip -d /content/AGT_rpg/
-#rm checkpoint.zip
 pip install Pillow==2.2.1
 pip install pyfiglet
 pip install colorama
@@ -10,10 +7,10 @@ pip install ebooklib
 pip install flask_ngrok
 pip install gpt_2_simple
 
-cd /content/app/
+cd /content/D-Epic/app/
 
 python db_create.py
 
-cd /content/app/engine/
+cd /content/D-Epic/app/engine/
 
 python language_m_downloader.py
